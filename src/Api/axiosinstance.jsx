bc5a213132bd04749ccc7cc4c5de6076a859b21e
abhishekc22 <abhishek234264@gmail.com>
2024-01-20 09:25:12 +0530

    baseurl

--- src/Api/axiosinstance.jsx
+++ src/Api/axiosinstance.jsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { jwtDecode } from "jwt-decode";
 
 
-const baseURL  = 'https://carway.vintiq.shop/';
+const baseURL  = import.meta.env.VITE_APP_BASE_URL
+
 
 const axiosInstance = axios.create({
     baseURL,
